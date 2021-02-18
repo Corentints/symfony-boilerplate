d5eb3156f49ab3ff860b0256f741bd804eb14ac6
Corentin <36472830+Corentints@users.noreply.github.com>
2021-02-18 18:48:03 +0100

    feat: add toBase64 extension

---
+++ ToBase64Extension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class Base64 extends AbstractExtension
+{
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('to_base_64', [$this, 'toBase64'])
+        ];
+    }
+
+    public function toBase64(string $url): string
+    {
+        $type = pathinfo($url, PATHINFO_EXTENSION);
+        $data = file_get_contents($url);
+        return 'data:image/' . $type . ';base64,' . base64_encode($data);
+    }
+}
