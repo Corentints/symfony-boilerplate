e8048418f472bddd8a542f855e5c2ff98260b150
Corentin <36472830+Corentints@users.noreply.github.com>
2021-02-28 20:40:14 +0100

    feat(datalistType): add datalistType

---
+++ DatalistType/DatalistType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+
+class DatalistType extends AbstractType
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function getParent(): string
+    {
+        return EntityType::class;
+    }
+}
